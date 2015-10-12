cdfddd85617ef1e274d64b1ab770a2fe1e6bc7ca
vuducdat <bka.dat@gmail.com>
2015-10-12 11:39:50 +0700

    refs#29-login and logout and create log

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012023333) do
+ActiveRecord::Schema.define(version: 20151012042208) do
+
+  create_table "user_logs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "log_data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "userlogs", force: :cascade do |t|
     t.integer  "user_id"
