3ecb040821cf32455a0fd1253950e7eb40b619b8
Dat Vu <bka.dat@gmail.com>
2015-11-02 04:44:49 +0000

    ref55#-ユーザープロフィール

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026021420) do
+ActiveRecord::Schema.define(version: 20151102040652) do
+
+  create_table "friends", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.boolean  "accepted"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "friends", ["friend_id"], name: "index_friends_on_friend_id"
 
   create_table "user_logs", force: :cascade do |t|
     t.integer  "user_id"
