b9d09556915e079f2ba65ceeb42af223522d18f1
Dat Vu <bka.dat@gmail.com>
2015-11-02 01:58:30 +0000

    ref41#-ユーザーテーブルの拡張

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012031757) do
+ActiveRecord::Schema.define(version: 20151026014521) do
 
   create_table "user_logs", force: :cascade do |t|
     t.integer  "user_id"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 20151012031757) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "gender"
+    t.integer  "age"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
